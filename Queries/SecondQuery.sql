d7c4dfae523e520917bbc87c980f2d4ee907a1ee
flesten-ali <fbada858@gmail.com>
2024-09-27 16:30:10 +0300

    Add second Query

---
+++ Queries/SecondQuery.sql
@@ -0,0 +1,30 @@
+WITH MoreThanOneUnReturnedBooks
+AS (
+	SELECT 
+	   COUNT(*) AS numOfUnReturned,
+	   b.BorrowerID
+	FROM 
+	   Borrowers AS b
+	JOIN 
+	   Loans AS l ON b.BorrowerID = l.BorrowerID
+	WHERE 
+	   l.DateReturned IS NULL
+	GROUP BY 
+	   b.BorrowerID
+	HAVING
+	   COUNT(*) >= 2
+	)
+
+SELECT
+   b.BorrowerID, 
+   CONCAT (b.FirstName, ' ', b.LastName) AS [Borrower Name], 
+   numOfUnReturned AS [Number of un returned books]
+FROM
+   MoreThanOneUnReturnedBooks AS unreturned
+JOIN 
+   Borrowers AS b ON b.BorrowerID = unreturned.BorrowerID
+
+
+
+
+ 
